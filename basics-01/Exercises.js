4da6dbebd083b3d838fcc27c7e56448da483fa49
Mega <mergenovezov987@gmail.com>
2025-11-11 14:04:20 +0800

    Created Exercises and did FCC task

---
+++ basics-01/Exercises.js
@@ -0,0 +1,16 @@
+//FreeCodeCamp ifElse
+/*
+Rewrite this if using the conditional operator '?':
+let result;
+
+if (a + b < 4) {
+  result = 'Below';
+} else {
+  result = 'Over';
+}
+ */
+let result;
+let a = 3;
+let b = 1;
+result = (a + b < 4) ? "Below" : "Over";
+
