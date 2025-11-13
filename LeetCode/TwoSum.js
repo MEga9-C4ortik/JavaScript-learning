a81b9748e6c91ce7dc76e50c81dc4d93de8aadf2
Mega <mergenovezov987@gmail.com>
2025-11-13 11:31:50 +0800

    LeetCode Two Sum 62ms

--- LeetCode/TwoSum.js
+++ LeetCode/TwoSum.js
@@ -8,12 +8,6 @@ You may assume that each input would have exactly one solution,
 You can return the answer in any order.
  */
 
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
-
 let twoSum = function(nums, target) {
     for(let i = 0; i < nums.length; i++) {
         for(let j = 1; j < nums.length; j++) {
