58e7f932d5cf979a6022807be390a3234ca9315a
Mega <mergenovezov987@gmail.com>
2025-11-14 14:42:18 +0800

    LeetCode IsPalindrome 5 ms 62.56 mb

---
+++ LeetCode/IsPalindromeNum.js
@@ -0,0 +1,14 @@
+// given an integer x, return true if x is a palindrome, and false otherwise.
+
+function isPalindrome (num){
+    let reverse = 0;
+    let original = num;
+    while(num > 0){
+        reverse = reverse * 10 + (num % 10);
+        num = Math.floor(num / 10);
+    }
+    return reverse === original ? true : false;
+}
+
+number = 10;
+console.log(isPalindrome(number));
