094c5caaf8d08faf6b06fd99ce6ea9f35d0063b2
Mega <mergenovezov987@gmail.com>
2025-11-18 17:31:49 +0800

    add button desigh change

---
+++ projects/WhatToWear/script.js
@@ -0,0 +1,10 @@
+function showSection( sectionName) {
+    const addButton = document.getElementById("addButton");
+
+    if(sectionName === "wardrobe"){
+        addButton.style.display = "block";
+    }
+    else{
+        addButton.style.display = "none";
+    }
+}
