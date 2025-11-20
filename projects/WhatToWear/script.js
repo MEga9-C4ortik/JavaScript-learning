8ba5eb664f5e8a4fb9cfa675e04034b1c6b576aa
Mega <mergenovezov987@gmail.com>
2025-11-20 17:24:00 +0800

    added wardrobe add button modal and form

--- projects/WhatToWear/script.js
+++ projects/WhatToWear/script.js
@@ -1,5 +1,10 @@
 let clothesArray = [];
 let savedOutfitsArray = [];
+const modal = document.getElementById("modal");
+const form = document.getElementById("form");
+const addButton = document.getElementById("addButton");
+const cancelButton = document.getElementById("cancelButton");
+const submitButton = document.getElementById("submitButton");
 
 function saveData(){
     localStorage.setItem('clothes', JSON.stringify(clothesArray));
@@ -45,5 +50,19 @@ document.getElementById("savedButton").addEventListener("click", () => {
     showSection("saved");
 });
 
+function openModal(){
+    document.getElementById("modal").style.display = "block";
+}
+
+function closeModal(){
+    document.getElementById("modal").style.display = "none";
+    document.getElementById("addForm").reset();
+}
+
+document.getElementById("addButton").addEventListener("click", () => openModal());
+document.getElementById("cancelButton").addEventListener("click", () => closeModal());
+document.getElementById("modalOverlay").addEventListener("click", () => closeModal());
+
 loadData();
 showSection("wardrobe");
+
