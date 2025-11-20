b254b2fedd6554f56c0b4bcb881ae7911164b161
Mega <mergenovezov987@gmail.com>
2025-11-20 15:41:13 +0800

    added section transition and local data load and save functions, also changed css

--- projects/WhatToWear/script.js
+++ projects/WhatToWear/script.js
@@ -1,9 +1,49 @@
-function showSection( sectionName) {
-    const addButton = document.getElementById("addButton");
+let clothesArray = [];
+let savedOutfitsArray = [];
 
-    if (sectionName === "wardrobe") {
-        addButton.style.display = "block";
-    } else {
-        addButton.style.display = "none";
+function saveData(){
+    localStorage.setItem('clothes', JSON.stringify(clothesArray));
+    localStorage.setItem('savedOutfits', JSON.stringify(savedOutfitsArray));
+}
+
+function loadData(){
+    const savedClothes = localStorage.getItem('clothes');
+
+    if(savedClothes){
+        clothesArray = JSON.parse(savedClothes);
+    }
+
+    const savedOutfits = localStorage.getItem('savedOutfits');
+    if(savedOutfits){
+        savedOutfitsArray = JSON.parse(savedOutfits);
     }
 }
+
+function showSection(sectionName){
+    document.getElementById("wardrobe").style.display = "none";
+    document.getElementById("generator").style.display = "none";
+    document.getElementById("saved").style.display = "none";
+
+    document.getElementById(sectionName).style.display = "block";
+
+    if( sectionName === "wardrobe") {
+        document.getElementById("addButton").classList.add("active");
+    }else if( sectionName === "generator") {
+        document.getElementById("generateButton").classList.add("active");
+    }
+}
+
+document.getElementById("wardrobeButton").addEventListener("click", () => {
+    showSection("wardrobe");
+});
+
+document.getElementById("generatorButton").addEventListener("click", () => {
+    showSection("generator");
+});
+
+document.getElementById("savedButton").addEventListener("click", () => {
+    showSection("saved");
+});
+
+loadData();
+showSection("wardrobe");
