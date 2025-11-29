07c4aba994ffbaccf2cbd3153f81ec8784ea0898
Mega <mergenovezov987@gmail.com>
2025-11-30 00:12:08 +0800

    devices types changes

---
+++ projects/Formula1Dashboard/script.js
@@ -0,0 +1,16 @@
+const sidenav = document.getElementById('sidenav');
+const hamburger = document.querySelector('.hamburger');
+
+function toggleSidebar() {
+    if (sidenav.open) {
+
+    }else{
+
+    }
+}
+
+function showSection() {
+
+}
+
+hamburger.addEventListener('click', toggleSidebar);
