22d8cb0724d8b845429cb5a79270270be86df409
Mega <mergenovezov987@gmail.com>
2025-11-30 00:18:54 +0800

    sidenav toggle function added

--- projects/Formula1Dashboard/script.js
+++ projects/Formula1Dashboard/script.js
@@ -1,16 +1,14 @@
 const sidenav = document.getElementById('sidenav');
 const hamburger = document.querySelector('.hamburger');
+const overlay = document.querySelector('.overlay');
 
-function toggleSidebar() {
-    if (sidenav.open) {
-
-    }else{
-
-    }
+function toggleSidenav() {
+    sidenav.classList.toggle('open');
+    overlay.classList.toggle('active');
 }
 
 function showSection() {
 
 }
 
-hamburger.addEventListener('click', toggleSidebar);
+hamburger.addEventListener('click', toggleSidenav);
