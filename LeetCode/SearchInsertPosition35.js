0a59d9cd8610ac4eb599390f503431f439048eed
Mega <megaovezov9@gmail.com>
2026-03-14 17:33:06 +0800

    Search Insert Position solved

---
+++ LeetCode/SearchInsertPosition35.js
@@ -0,0 +1,26 @@
+// 35. Search Insert Position
+
+//runtime 0ms (beats 100%), memory 55.03 mb (beats 8.90%)
+// speed O(logN) | memory O(n)
+
+var searchInsert = function(nums, key) {
+    let lo = 0;
+    let hi = nums.length - 1;
+    let mi = -1;
+
+    while (lo <= hi) {
+        mi = Math.floor(lo + (hi - lo) / 2);
+        if(nums[mi] === key)
+            return mi;
+        else if (nums[mi] > key)
+            hi = mi - 1;
+        else if (nums[mi] < key)
+            lo = mi + 1;
+    }
+
+    return lo;
+};
+
+console.log(searchInsert([1,2,3,4,5,6,7,8], 6)); //5
+console.log(searchInsert([1,2,3,4,5,6,7,8], 1)); //0
+console.log(searchInsert([1,2,3,4,5,6,7,8], 9)); //8
